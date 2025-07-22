3342575145ca9f7e29148d227a555f37e2d59df9
Steven <ssaw1@asu.edu>
2025-07-22 12:19:37 -0700

    update

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict"; // Using Strict Mode
+
+$(document).ready(function() {
+    // Your jQuery code here
+    $('nav a').on('click', function() {
+        alert('Navigation link clicked!');
+    });
+});
