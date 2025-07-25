8c02a357cd6e3c02e0999523dafdfdd46c86908c
Steven <ssaw1@asu.edu>
2025-07-24 22:05:04 -0700

    update

--- main.js
+++ main.js
@@ -1,9 +1,2 @@
 "use strict"; // Using Strict Mode
 
-// Slide Show of student pictures, who have worked at Education at Work
-$(document).ready(function(){
-  $('.slideshow').slick({
-    autoplay: true,
-    dots: true
-  });
-});
