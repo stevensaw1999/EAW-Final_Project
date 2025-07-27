fccc3510fc7a674a76d76f8faeaab694cc1df262
Steven <ssaw1@asu.edu>
2025-07-27 10:30:29 -0700

    update

--- main.js
+++ main.js
@@ -1,2 +1,25 @@
 "use strict"; // Using Strict Mode
 
+$(document).ready(function() {
+    // Initialize Employee Slideshow
+    $('.employee-slideshow').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+        arrows: true,
+        fade: true,
+        speed: 500,
+        cssEase: 'ease-in-out',
+        pauseOnHover: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    dots: true
+                }
+            }
+        ]
+    });
+});
+
