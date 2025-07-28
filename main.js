aeeead409d7a8b10937d5e3ba260f2a990ffe6ba
Steven <ssaw1@asu.edu>
2025-07-28 12:06:15 -0700

    Update

--- main.js
+++ main.js
@@ -9,7 +9,36 @@ $(document).ready(function() {
         animate: 300
     });
 
-    $("")
+    // Initialize Slick Carousel Functionality for partners section
+    $('.partner-carousel').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: false,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 769, 
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
 });
 
-// Section for Loading API Data from Google Maps
+// Section for Loading Slick Carousel
+
